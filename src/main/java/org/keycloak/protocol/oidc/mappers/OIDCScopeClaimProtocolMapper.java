d513a0af2014569747f708a8aa04e1192692cbff
Lorenzo Rossoni <l.rossoni@selecta.ve.it>
2024-02-13 13:13:00 +0100

    initial commit

--- src/main/java/org/keycloak/protocol/oidc/mappers/OIDCScopeClaimProtocolMapper.java
+++ src/main/java/org/keycloak/protocol/oidc/mappers/OIDCScopeClaimProtocolMapper.java
@@ -6,52 +6,37 @@ import java.util.List;
 import java.util.Map;
 
 import org.keycloak.models.ClientSessionContext;
-import org.keycloak.models.GroupModel;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.ProtocolMapperModel;
 import org.keycloak.models.UserSessionModel;
-import org.keycloak.models.utils.ModelToRepresentation;
 import org.keycloak.protocol.oidc.OIDCLoginProtocol;
 import org.keycloak.provider.ProviderConfigProperty;
 import org.keycloak.rar.AuthorizationRequestContext;
 import org.keycloak.representations.IDToken;
 
-public class OIDCScopeGroupProtocolMapper extends AbstractOIDCProtocolMapper
+public class OIDCScopeClaimProtocolMapper extends AbstractOIDCProtocolMapper
         implements OIDCAccessTokenMapper, OIDCIDTokenMapper, UserInfoTokenMapper {
     private static final List<ProviderConfigProperty> configProperties = new ArrayList<ProviderConfigProperty>();
 
-    private static final String FULL_PATH = "fullPath";
     private static final String SCOPE = "scope";
 
-    private static final String FULL_PATH_LABEL = "oidc-scope-group-protocol-mapper.full-path.label";
-    private static final String FULL_PATH_HELP_TEXT = "oidc-scope-group-protocol-mapper.full-path.tooltip";
-    private static final String SCOPE_LABEL = "oidc-scope-group-protocol-mapper.scope.label";
-    private static final String SCOPE_HELP_TEXT = "oidc-scope-group-protocol-mapper.scope.tooltip";
+    private static final String SCOPE_LABEL = "oidc-scope-claim-protocol-mapper.scope.label";
+    private static final String SCOPE_HELP_TEXT = "oidc-scope-claim-protocol-mapper.scope.tooltip";
 
-    public static final String PROVIDER_ID = "oidc-scope-group-protocol-mapper";
+    public static final String PROVIDER_ID = "oidc-scope-claim-protocol-mapper";
 
     static {
         OIDCAttributeMapperHelper.addTokenClaimNameConfig(configProperties);
-
-        ProviderConfigProperty fullPathProperty = new ProviderConfigProperty();
-        fullPathProperty.setName(FULL_PATH);
-        fullPathProperty.setLabel(FULL_PATH_LABEL);
-        fullPathProperty.setType(ProviderConfigProperty.BOOLEAN_TYPE);
-        fullPathProperty.setDefaultValue("true");
-        fullPathProperty.setHelpText(FULL_PATH_HELP_TEXT);
-
-        configProperties.add(fullPathProperty);
 
         ProviderConfigProperty scopeProperty = new ProviderConfigProperty();
         scopeProperty.setName(SCOPE);
         scopeProperty.setLabel(SCOPE_LABEL);
         scopeProperty.setType(ProviderConfigProperty.STRING_TYPE);
-        scopeProperty.setDefaultValue("group");
         scopeProperty.setHelpText(SCOPE_HELP_TEXT);
 
         configProperties.add(scopeProperty);
 
-        OIDCAttributeMapperHelper.addIncludeInTokensConfig(configProperties, OIDCScopeGroupProtocolMapper.class);
+        OIDCAttributeMapperHelper.addIncludeInTokensConfig(configProperties, OIDCScopeClaimProtocolMapper.class);
     }
 
     @Override
@@ -61,7 +46,7 @@ public class OIDCScopeGroupProtocolMapper extends AbstractOIDCProtocolMapper
 
     @Override
     public String getDisplayType() {
-        return "Scope-based Group Membership";
+        return "Scope-based claim mapper";
     }
 
     @Override
@@ -76,7 +61,7 @@ public class OIDCScopeGroupProtocolMapper extends AbstractOIDCProtocolMapper
 
     @Override
     public String getHelpText() {
-        return "Map scope to user group membership";
+        return "Map scope dynamic value to claim";
     }
 
     @Override
@@ -85,22 +70,14 @@ public class OIDCScopeGroupProtocolMapper extends AbstractOIDCProtocolMapper
         AuthorizationRequestContext authorizationRequestContext = clientSessionCtx.getAuthorizationRequestContext();
 
         String scopeName = mappingModel.getConfig().get(SCOPE);
-        String groupName = authorizationRequestContext.getAuthorizationDetailEntries()
+        String claimValue = authorizationRequestContext.getAuthorizationDetailEntries()
                 .stream()
                 .filter(d -> d.getClientScope().getName().equals(scopeName))
                 .map(d -> d.getDynamicScopeParam())
                 .findFirst().orElse(null);
 
-        if (groupName != null) {
-            String membership = userSession.getUser().getGroupsStream()
-                    .filter(g -> g.getName().equalsIgnoreCase(groupName))
-                    .map(useFullPath(mappingModel)
-                            ? ModelToRepresentation::buildGroupPath
-                            : GroupModel::getName)
-                    .findFirst().orElse(null);
-            if (membership != null) {
-                OIDCAttributeMapperHelper.mapClaim(idToken, mappingModel, membership);
-            }
+        if (claimValue != null) {
+            OIDCAttributeMapperHelper.mapClaim(idToken, mappingModel, claimValue);
         }
     }
 
@@ -126,9 +103,5 @@ public class OIDCScopeGroupProtocolMapper extends AbstractOIDCProtocolMapper
 
         mapper.setConfig(config);
         return mapper;
-    }
-
-    private static boolean useFullPath(ProtocolMapperModel mappingModel) {
-        return "true".equals(mappingModel.getConfig().get(FULL_PATH));
     }
 }
